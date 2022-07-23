3a56cb5c2bcefd6008c35d09a691883bf3c3de9c
Md Ridwan <72821054+mdridwwan@users.noreply.github.com>
2022-07-23 22:31:12 +0600

    last

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const cors = require('cors');
 const expressServer = http.createServer(app);
 const {Server} = require('socket.io');
 
+const port = 4500 || process.env.PORT;
 const io = new Server(expressServer);
+
+app.use(cors());
+
+app.get('/', (req,res)=>{
+    res.send('hellow server')
+})
+
+io.on('connection', (sockets)=>{
+    console.log("new user connected");
+    sockets.on('disconnect', ()=>{
+        console.log("user disconnected");
+    })
+})
+
+
+expressServer.listen(port, ()=>{
+    console.log("server is running port: http://localhost:", (port))
+})
